16c4e9513ddb1b5ec26034203fca61caafb8f637
Drakecub <drakecubz@gmail.com>
2026-04-09 16:37:51 -0600

    Changed cors URL for internet access

--- src/index.ts
+++ src/index.ts
@@ -6,7 +6,7 @@ import uploadRouter from "./routes/upload";
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(cors({ origin: "http://localhost:4200" }));
+app.use(cors({ origin: "https://DrakeCub.github.io" }));
 app.use(express.json());
 
 app.use("/api/upload", uploadRouter);
